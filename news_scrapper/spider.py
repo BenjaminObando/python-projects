992715b57afecfe4fe4c3c8a75cc8165ecf8d2a0
Benjamin Obando <48965824+BenjaminObando@users.noreply.github.com>
2020-04-19 19:37:30 -0400

    Add files via upload

---
+++ news_scrapper/spider.py
@@ -0,0 +1,134 @@
+import sqlite3
+import urllib.error
+import ssl
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from test import get_noticia
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+from datetime import date
+
+today=date.today()
+
+
+conn = sqlite3.connect("data/noticias.sqlite")
+cur = conn.cursor()
+
+
+
+##### tabla con portales de noticias
+urls={"emol":"https://www.emol.com",
+      "la_tercera":"https://www.latercera.com",
+      "cnn_chile":"https://www.cnnchile.com"}
+
+cur.execute('''CREATE TABLE IF NOT EXISTS portales_noticias
+    (id INTEGER PRIMARY KEY, name TEXT UNIQUE,url TEXT UNIQUE)''')
+
+for name,url in urls.items():
+    print(name,url)
+    cur.execute('INSERT OR IGNORE INTO portales_noticias ( name, url) VALUES ( ?, ? )', ( name, url ) )
+conn.commit()
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS dias_ejecutados
+    (id INTEGER PRIMARY KEY, ex_dt DATETIME, portal_noticias_id INTEGER)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS noticias
+            (id INTEGER PRIMARY KEY, portal_noticias_id INT, url TEXT UNIQUE,
+             contenido TEXT, html TEXT)''')
+
+
+#cur.execute('''CREATE TABLE IF NOT EXISTS noticias
+#    (id INTEGER PRIMARY KEY, titulo TEXT,portal_noticias_id INTEGER, url TEXT UNIQUE,
+#     descripcion TEXT, contenido TEXT, error TEXT, html TEXT)''')
+
+# Check to see if we are already in progress...
+
+
+
+cur.execute('SELECT ex_dt FROM dias_ejecutados ORDER BY ex_dt DESC')
+row = cur.fetchone()
+if row is None:
+    print("No hay noticias extraidas")
+else :
+    print("Ultimo dia de ejecucion:", row)
+
+# Get the current webs
+cur.execute('''SELECT url,id FROM portales_noticias''')
+webs = list()
+for row in cur:
+    webs.append((str(row[0]),row[1]))
+
+print(webs)
+
+many = 0
+for url, id_portal in webs:
+    
+    try:
+        document = urlopen(url, context=ctx)
+
+        html = document.read()
+        if document.getcode() != 200 :
+            print("Error on page: ",document.getcode())
+           
+        if 'text/html' != document.info().get_content_type() :
+            print("Ignore non text/html page")
+            continue
+
+
+        soup = BeautifulSoup(html, "html.parser")
+    except KeyboardInterrupt:
+        print('')
+        print('Program interrupted by user...')
+        break
+    except:
+        print("Unable to retrieve or parse page")
+        continue
+
+    conn.commit()
+
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    count = 0
+    for tag in tags:
+        href = tag.get('href', None)
+        if ( href is None ) : continue
+        # Resolve relative references like href="/contact"
+        up = urlparse(href)
+        if ( len(up.scheme) < 1 ) :
+            href = urljoin(url, href)
+        ipos = href.find('#')
+        if ( ipos > 1 ) : href = href[:ipos]
+        if ( href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif') ) : continue
+        if ( href.endswith('/') ) : href = href[:-1]
+        # print href
+        if ( len(href) < 1 ) : continue
+
+		# Check if the URL is in any of the webs
+        found = False
+        for web in url:
+            if ( href.startswith(web) ) :
+                found = True
+                break
+        if not found : continue
+        print(href)
+        
+        try:
+            parrafos=get_noticia(href)
+            parrafos=" ".join(parrafos)
+        except:
+            parrafos="error"
+        cur.execute('INSERT OR IGNORE INTO noticias (portal_noticias_id,url, contenido, html) VALUES ( ?, ?, ?, ? )', ( id_portal, href,parrafos,html ) )
+        count = count + 1
+        conn.commit()
+    cur.execute("""INSERT OR IGNORE INTO dias_ejecutados (ex_dt ,portal_noticias_id) 
+                    VALUES ( ?, ?)""", ( id_portal, today))
+
+    print(count)
+
+cur.close()
---
+++ news_scrapper/test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 21:00:02 2020
+
+@author: benjaminobando
+"""
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from bs4 import NavigableString,Tag
+import re
+
+def get_noticia(url):
+    
+    m = re.search('www.(.+?).com', url)
+    found = m.group(1)
+    
+    document = urlopen(url)
+    
+    html = document.read()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    lista_span=[]
+    
+    if found=="emol":
+        iterable=soup.find_all("div",{"id":"cuDetalle_cuTexto_textoNoticia" })
+        
+    elif found in ["latercera","cnnchile"]:
+        iterable=soup.find_all("p")
+        
+    for body_child in iterable:
+        if isinstance(body_child, NavigableString):
+            continue
+        if isinstance(body_child, Tag):
+            lista_span.append(body_child.getText())    
+    return(lista_span)
+
+url="https://www.emol.com/noticias/Nacional/2020/04/10/982731/Gobierno-variables-cuarentena-coronavirus-Chile.html"
+url="https://www.emol.com/noticias/Nacional/2020/04/10/982724/Gobierno-Coronavirus-Balance.html"
+url="https://www.emol.com/noticias/Nacional/2020/04/11/982770/Minsal-Coronavirus-Balance.html"
+url="https://www.latercera.com/mundo/noticia/oms-advierte-sobre-peligro-de-levantar-restricciones/RPABRLQNH5BXPAEU2WG3JEFLFA/"
+get_noticia(url)
+    
+#text = 'gfgfdAAA1234ZZZuijjk'
+
